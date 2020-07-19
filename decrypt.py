dc11da4dda65fe2b7394fbb73d928397a3d98408
Yishu See <yishuyishu@gmail.com>
2020-07-19 14:09:51 +0800

    Add 2/6

--- decrypt.py
+++ decrypt.py
@@ -33,4 +33,29 @@ def one():
     two.close()
 
 
-one()
+def two():
+    txt = open("./payload_2.txt", "r")
+    txt_string = txt.read()
+    txt_decode = base64.a85decode(txt_string, adobe=True)
+
+    good_bits = []
+    for b in txt_decode:
+        parity = b & 1
+        count = 0
+        sig = format(b, "08b")[:-1]
+        for bit in sig:
+            if bit == "1":
+                count += 1
+        if count % 2 == parity:
+            good_bits.extend(sig)
+
+    decoded = []
+    for b in range(int(len(good_bits) / 8)):
+        byte = "".join(good_bits[b * 8 : (b + 1) * 8])
+        decoded.append(chr(int(byte, 2)))
+    three = open("./3.txt", "w")
+    three.write("".join(decoded))
+    three.close()
+
+
+two()
