0f39d0944d7debcfb33b17ace0e6db8fdaa19cbe
Yishu See <yishuyishu@gmail.com>
2020-07-19 14:19:42 +0800

    Remove decrypted files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # toms-data-onion
+
+Working through https://www.tomdalling.com/toms-data-onion/
+
+For each layer, I moved the payload into its own file `payload_*.txt` to easily read from it. If you are following along with the solution here, remember to do the same.
--- decrypt.py
+++ decrypt.py
@@ -2,17 +2,17 @@ import base64
 
 
 def start():
-    txt = open("./payload.txt", "r")
+    txt = open("./data/payload.txt", "r")
     txt_string = txt.read()
     txt_decode = base64.a85decode(txt_string, adobe=True)
 
-    one = open("./1.txt", "w")
+    one = open("./decrypted/1.txt", "w")
     one.write(txt_decode.decode("UTF-8"))
     one.close()
 
 
 def one():
-    txt = open("./payload_1.txt", "r")
+    txt = open("./decrypted/payload_1.txt", "r")
     txt_string = txt.read()
     txt_decode = base64.a85decode(txt_string, adobe=True)
 
@@ -28,13 +28,13 @@ def one():
         bit = b & 1
         decoded[i] = bit << 7 | b >> 1
 
-    two = open("./2.txt", "w")
+    two = open("./decrypted/2.txt", "w")
     two.write(bytes(decoded).decode("utf8"))
     two.close()
 
 
 def two():
-    txt = open("./payload_2.txt", "r")
+    txt = open("./decrypted/payload_2.txt", "r")
     txt_string = txt.read()
     txt_decode = base64.a85decode(txt_string, adobe=True)
 
@@ -53,9 +53,15 @@ def two():
     for b in range(int(len(good_bits) / 8)):
         byte = "".join(good_bits[b * 8 : (b + 1) * 8])
         decoded.append(chr(int(byte, 2)))
-    three = open("./3.txt", "w")
+    three = open("./decrypted/3.txt", "w")
     three.write("".join(decoded))
     three.close()
 
 
-two()
+def three():
+    txt = open("./decrypted/payload_3.txt", "r")
+    txt_string = txt.read()
+    txt_decode = base64.a85decode(txt_string, adobe=True)
+
+
+three()
